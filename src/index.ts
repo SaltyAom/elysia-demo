83d220ea23d884a77a0b2dfdd287ed779ce39ec9
saltyaom <saltyaom@gmail.com>
2022-12-16 12:11:37 +0700

    :tada: feat: init

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { Elysia } from 'elysia'
+import { swagger } from '@elysiajs/swagger'
+
+const app = new Elysia()
+    .use(swagger())
+    .get('/', () => 'Hello Elysia')
+    .listen(3000)
+
+console.log(
+    `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+)
